35a86158d3cc7a72b921d0d6b2595b3e5734ebba
marina40295 <merpuck730@hotmail.com>
2020-04-01 15:25:00 -0400

    new signup Ok message

---
+++ src/pages/signupSuccessful.js
@@ -0,0 +1,12 @@
+import React from 'react';
+ 
+export const signUpSuccess = () => {
+    return (
+       <div>
+          <h1>Sign up complete!</h1>
+          <p>We'll see you at tryouts!</p>
+       </div>
+    );
+}
+ 
+export default signUpSuccess;
