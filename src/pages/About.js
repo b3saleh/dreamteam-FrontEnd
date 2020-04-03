3ed74db5e500b6201530d6bc3749fc8f3ca56b33
marina40295 <merpuck730@hotmail.com>
2020-04-03 10:44:57 -0400

    about page changes

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,11 @@ import React from 'react';
  
 export const About = () => {
     return (
-       <div>
+       <div className="aboutBlurb">
           <h1>About US</h1>
-          <p>This page is under construction</p>
+          <p>DreamTeam was designed for the University of Waterloo Ultimate Frisbee team, to  provide the coaches and captains of a team with a way to track athlete’s performance and development in a tryout setting. 
+          When a team is hosting tryouts, coaches and captains need to evaluate various athletes and later access that information to build a strong and balanced team. </p>
+          <p> DreamTeam is here to help!</p>
        </div>
     );
 }
